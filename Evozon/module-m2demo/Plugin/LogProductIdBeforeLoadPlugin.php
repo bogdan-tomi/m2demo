19e81592e793dfc1f9f8ad10cc4bc6354d981669
Bogdan Tomi <bogdan.tomi@evozon.com>
2020-09-17 12:47:17 +0300

    Update composer.json and file structure

---
+++ Evozon/module-m2demo/Plugin/LogProductIdBeforeLoadPlugin.php
@@ -0,0 +1,28 @@
+<?php declare(strict_types=1);
+
+namespace Evozon\M2Demo\Plugin;
+
+use Magento\Catalog\Api\ProductRepositoryInterface;
+use Psr\Log\LoggerInterface;
+
+class LogProductIdBeforeLoadPlugin
+{
+    /**
+     * @var LoggerInterface
+     */
+    private LoggerInterface $logger;
+
+    public function __construct(LoggerInterface $logger)
+    {
+        $this->logger = $logger;
+    }
+
+    public function beforeGetById(ProductRepositoryInterface $subject,
+                                  $productId,
+                                  $editMode = false,
+                                  $storeId = null,
+                                  $forceReload = false)
+    {
+        $this->logger->info("Logging before retrieving product by ID $productId");
+    }
+}
---
+++ Evozon/module-m2demo/registration.php
@@ -0,0 +1,9 @@
+<?php
+
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(
+    ComponentRegistrar::MODULE,
+    'Evozon_M2Demo',
+    __DIR__
+);
