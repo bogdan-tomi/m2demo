3f743b5e824e61115b6754f0ed0284884a5a0ef4
Bogdan Tomi <bogdan.tomi@evozon.com>
2020-09-18 16:21:58 +0300

    Add blocks and layout changes

---
+++ Evozon/module-m2demo/Block/Hello.php
@@ -0,0 +1,15 @@
+<?php declare(strict_types=1);
+
+namespace Evozon\M2Demo\Block;
+
+use Magento\Framework\View\Element\Template;
+//use Magento\Framework\View\Element\Template\Context;
+
+class Hello extends Template
+{
+
+//    public function __construct(Context $context, array $data = [])
+//    {
+//        parent::__construct($context, $data);
+//    }
+}
---
+++ Evozon/module-m2demo/Block/Hello/Child.php
@@ -0,0 +1,10 @@
+<?php declare(strict_types=1);
+
+namespace Evozon\M2Demo\Block\Hello;
+
+use Magento\Framework\View\Element\Template;
+
+class Child extends Template
+{
+
+}
