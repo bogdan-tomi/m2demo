50cc794883a742a632c6f208e14efa897b6ad245
Bogdan Tomi <bogdan.tomi@evozon.com>
2020-11-10 14:17:12 +0200

    DOM update with RequireJS modules

--- Evozon/module-m2demo/view/frontend/web/js/requirejs-example.js
+++ Evozon/module-m2demo/view/frontend/web/js/requirejs-example.js
@@ -1,7 +1,8 @@
 define(function(){
     'use strict';
 
-    return function (config) {
+    return function (config, element) {
         console.log('Evozon -> Loaded via RequireJS AMD module function', config);
+        element.innerHTML = 'text added from AMD module with JS';
     }
 })
