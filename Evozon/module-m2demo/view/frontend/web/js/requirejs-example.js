b3681a10971f6bd666e301e20887792235b5cb1c
Bogdan Tomi <bogdan.tomi@evozon.com>
2020-11-10 12:31:34 +0200

    Add JS simple AMD module

--- Evozon/module-m2demo/view/frontend/web/js/requirejs-example.js
+++ Evozon/module-m2demo/view/frontend/web/js/requirejs-example.js
@@ -1,1 +1,7 @@
-console.log('Evozon -> Loaded via RequireJS');
+define(function(){
+    'use strict';
+
+    return function() {
+        console.log('Evozon -> Loaded via RequireJS AMD module function');
+    }
+})
