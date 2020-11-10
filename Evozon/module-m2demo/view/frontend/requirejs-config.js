e8896e32d2f2f4402beaf5d8ee70c94282ba5211
Bogdan Tomi <bogdan.tomi@evozon.com>
2020-11-10 16:43:57 +0200

    Add alias for custom RequireJS module

---
+++ Evozon/module-m2demo/view/frontend/requirejs-config.js
@@ -0,0 +1,9 @@
+// this belongs under the area code folder (i.e. frontend), not under the web folder
+var config = {
+    map: {
+        // * represents which RequireJS modules can use our alias, stands for all
+        '*': {
+            shortcut_dependency: 'Evozon_M2Demo/js/requirejs-dependency'
+        }
+    }
+};
