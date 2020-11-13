a5b4425cb804f85f4227f557da68cc74ab1d2da6
Bogdan Tomi <bogdan.tomi@evozon.com>
2020-11-13 18:05:55 +0200

    Add knockoutjs computed observable

--- Evozon/module-m2demo/view/frontend/web/js/plain-view-model.js
+++ Evozon/module-m2demo/view/frontend/web/js/plain-view-model.js
@@ -1,25 +1,42 @@
-define(['ko'], function (ko) {
+define(['ko', 'jquery'], function (ko, $) {
     'use strict';
 
     return function (config) {
-        // set the title as observable so any changes to its value are reflected on frontend in all the property occurrences
-        const title = ko.observable('Test title for a simple knockoutjs view model');
 
-        title.subscribe(function (oldValue) {
+        let currencyInfo = ko.observable();
+        $.getJSON(config.base_url + 'rest/V1/directory/currency', currencyInfo); // we cannot pass a value because the observable would be updated
+        const viewModel = {
+            // set the title as observable so any changes to its value are reflected on frontend in all the property occurrences
+            title: ko.observable('Test title for a simple knockoutjs view model'),
+            label: ko.observable('Currency Info')
+        };
+
+        viewModel.title.subscribe(function (oldValue) {
             console.log('Will be changed from', oldValue)
         }, this, 'beforeChange');
 
-        title.subscribe(function (newValue) {
+        viewModel.title.subscribe(function (newValue) {
             console.log('Changed to', newValue);
         });
-        return {
-            title: title,
-            getTitle: function () {
-                return title();
-            },
-            getConfig: function () {
-                return config;
+
+        // any time an observable is changed inside the computed function, the function is re-called.
+        // we need to delay the computed in order for the viewModel to already be created,
+        // otherwise cannot read property of undefined errors occur (for the label in this case)
+        viewModel.output = ko.computed(function () {
+            if (!currencyInfo()) {
+                return '...loading';
             }
-        }
+            return this.label() + ':' + JSON.stringify(currencyInfo(), null, 2);
+        }.bind(viewModel));
+
+        viewModel.getTitle = function () {
+            return this.title();
+        };
+
+        viewModel.getConfig = function () {
+            return config;
+        };
+
+        return viewModel;
     }
 });
