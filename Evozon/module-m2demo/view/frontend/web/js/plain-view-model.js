ad4eacd224462c1dd95f73f32ebbdeeeef03aa78
Bogdan Tomi <bogdan.tomi@evozon.com>
2020-11-13 16:09:57 +0200

    Add subscribers to state changes of knockoutjs observables

--- Evozon/module-m2demo/view/frontend/web/js/plain-view-model.js
+++ Evozon/module-m2demo/view/frontend/web/js/plain-view-model.js
@@ -2,11 +2,20 @@ define(['ko'], function (ko) {
     'use strict';
 
     return function (config) {
+        // set the title as observable so any changes to its value are reflected on frontend in all the property occurrences
+        const title = ko.observable('Test title for a simple knockoutjs view model');
+
+        title.subscribe(function (oldValue) {
+            console.log('Will be changed from', oldValue)
+        }, this, 'beforeChange');
+
+        title.subscribe(function (newValue) {
+            console.log('Changed to', newValue);
+        });
         return {
-            // set the title as observable so any changes to its value are reflected on frontend in all the property occurrences
-            title: ko.observable('Test title for a simple knockoutjs view model'),
+            title: title,
             getTitle: function () {
-                return this.title();
+                return title();
             },
             getConfig: function () {
                 return config;
