3c7eb66152bffd51592cdfa8e1036ff55996d36b
Bogdan Tomi <bogdan.tomi@evozon.com>
2020-11-13 15:23:41 +0200

    Set view model property as observable

--- Evozon/module-m2demo/view/frontend/web/js/plain-view-model.js
+++ Evozon/module-m2demo/view/frontend/web/js/plain-view-model.js
@@ -1,11 +1,12 @@
-define(function () {
+define(['ko'], function (ko) {
     'use strict';
 
     return function (config) {
         return {
-            title: 'Test title for a simple knockoutjs view model',
+            // set the title as observable so any changes to its value are reflected on frontend in all the property occurrences
+            title: ko.observable('Test title for a simple knockoutjs view model'),
             getTitle: function () {
-                return this.title;
+                return this.title();
             },
             getConfig: function () {
                 return config;
