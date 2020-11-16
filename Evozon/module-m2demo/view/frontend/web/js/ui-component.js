8046128bc102ff271422fe1062810450233c8dca
Bogdan Tomi <bogdan.tomi@evozon.com>
2020-11-16 17:14:01 +0200

    Add UiComponent template

--- Evozon/module-m2demo/view/frontend/web/js/ui-component.js
+++ Evozon/module-m2demo/view/frontend/web/js/ui-component.js
@@ -4,7 +4,8 @@ define(['uiElement'], function (UiElement) {
     return UiElement.extend({
         // defaults can be used to set specific default values
         defaults: {
-            label: "My first defaults UiComponent! (1)"
+            label: "My first defaults UiComponent! (1)",
+            template: "Evozon_M2Demo/ui-component-template" // this looks for a ui-component-template HTML file under frontend/web/template
         },
         label: "My first UiComponent!"
     })
