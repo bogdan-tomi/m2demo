845834c653b2c1f7b01e69def6f8f833ba60e0b6
Bogdan Tomi <bogdan.tomi@evozon.com>
2020-11-16 16:57:16 +0200

    Add defaults to UiComponent properties

--- Evozon/module-m2demo/view/frontend/web/js/ui-component.js
+++ Evozon/module-m2demo/view/frontend/web/js/ui-component.js
@@ -2,6 +2,10 @@ define(['uiElement'], function (UiElement) {
     'use strict';
 
     return UiElement.extend({
+        // defaults can be used to set specific default values
+        defaults: {
+            label: "My first defaults UiComponent! (1)"
+        },
         label: "My first UiComponent!"
     })
 });
