1bf6490a4eb295c46e46ddd70ded90aef596b9d2
Bogdan Tomi <bogdan.tomi@evozon.com>
2020-11-17 12:55:45 +0200

    Declaring es5 observables in JS UiComponent with default tracks

--- Evozon/module-m2demo/view/frontend/web/js/ui-component.js
+++ Evozon/module-m2demo/view/frontend/web/js/ui-component.js
@@ -5,8 +5,17 @@ define(['uiElement'], function (UiElement) {
         // defaults can be used to set specific default values
         defaults: {
             label: "My first defaults UiComponent! (1)",
-            template: "Evozon_M2Demo/ui-component-template" // this looks for a ui-component-template HTML file under frontend/web/template
+            template: "Evozon_M2Demo/ui-component-template", // this looks for a ui-component-template HTML file under frontend/web/template
+            tracks: {
+                label: true,
+                value: true
+                // use the tracks property to make specific UiComponent properties observable
+            }
         },
-        label: "My first UiComponent!"
+        label: "My first UiComponent!",
+        value: 15,
+        someFunc: function () {
+            return this.value * 10;
+        }
     })
 });
