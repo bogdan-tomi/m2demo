2e430ac85e8f1487dc05cab7073b96c8b5fb7064
Bogdan Tomi <bogdan.tomi@evozon.com>
2020-12-07 13:53:42 +0200

    Add alternative ajax caching using browser session storage

--- Evozon/module-cache/Controller/Block/Index.php
+++ Evozon/module-cache/Controller/Block/Index.php
@@ -32,7 +32,8 @@ class Index implements HttpGetActionInterface
         $result->addHandle('evozon_cache_ajax_visitor_county');
 
         // set the time-to-live to 20 seconds on the http response, so we can cache the ajax response in the browser http cache
-        $this->httpResponse->setPrivateHeaders(20);
+        // commented so the browser session storage is used instead
+//        $this->httpResponse->setPrivateHeaders(20);
 
         return $result; // remember to return result
     }
--- Evozon/module-cache/view/frontend/web/js/visitor-county.js
+++ Evozon/module-cache/view/frontend/web/js/visitor-county.js
@@ -1,13 +1,20 @@
 define(['jquery'], function ($) {
     'use strict';
 
+    const key = 'evozon-cache-visitor-county';
+
     return function (config, element) {
-        $.get({
-            url: '/evozon-visitor-county/block',
-            cache: true,
-            success: function (result) {
-                element.innerHTML = result;
-            }
-        });
+        if (window.sessionStorage.getItem(key)) {
+            element.innerHTML = window.sessionStorage.getItem(key);
+        } else {
+            $.get({
+                url: '/evozon-visitor-county/block',
+                cache: true,
+                success: function (result) {
+                    window.sessionStorage.setItem(key, result);
+                    element.innerHTML = result;
+                }
+            });
+        }
     }
 });
