c244bd54d096baae19d1689617185bb1ad8e17b4
Bogdan Tomi <bogdan.tomi@evozon.com>
2021-01-06 12:53:43 +0200

    Add data object processor for graphQL result

--- Evozon/module-cache-graph-ql/Model/Resolver/FavouritePetProductsResolver.php
+++ Evozon/module-cache-graph-ql/Model/Resolver/FavouritePetProductsResolver.php
@@ -2,11 +2,16 @@
 
 namespace Evozon\CacheGraphQl\Model\Resolver;
 
+use Magento\Catalog\Api\Data\ProductInterface;
 use Magento\Catalog\Model\ProductRepository;
 use Magento\Framework\Api\SearchCriteriaBuilder;
 use Magento\Framework\GraphQl\Config\Element\Field;
 use Magento\Framework\GraphQl\Query\ResolverInterface;
 use Magento\Framework\GraphQl\Schema\Type\ResolveInfo;
+use Magento\Framework\Reflection\DataObjectProcessor;
+
+use function array_map as map;
+use function array_values as values;
 
 class FavouritePetProductsResolver implements ResolverInterface
 {
@@ -19,20 +24,33 @@ class FavouritePetProductsResolver implements ResolverInterface
      * @var SearchCriteriaBuilder
      */
     private SearchCriteriaBuilder $searchCriteriaBuilder;
+    /**
+     * @var DataObjectProcessor
+     */
+    private DataObjectProcessor $dataObjectProcessor;
 
-    public function __construct(ProductRepository $productRepository, SearchCriteriaBuilder $searchCriteriaBuilder)
-    {
+    public function __construct(
+        ProductRepository $productRepository,
+        SearchCriteriaBuilder $searchCriteriaBuilder,
+        DataObjectProcessor $dataObjectProcessor
+    ) {
         $this->productRepository = $productRepository;
         $this->searchCriteriaBuilder = $searchCriteriaBuilder;
+        $this->dataObjectProcessor = $dataObjectProcessor;
+    }
+
+    private function productDtoToArray(ProductInterface $product): array
+    {
+        return $this->dataObjectProcessor->buildOutputDataArray($product, ProductInterface::class);
     }
 
     public function resolve(Field $field, $context, ResolveInfo $info, array $value = null, array $args = null)
     {
-        $this->searchCriteriaBuilder->addFilter('name', chr(rand(97,122)) . '%', 'like');
+        $this->searchCriteriaBuilder->addFilter('name', chr(rand(97, 122)) . '%', 'like');
+        $this->searchCriteriaBuilder->setPageSize(5);
         $result = $this->productRepository->getList($this->searchCriteriaBuilder->create());
         $products = $result->getItems();
 
-        // todo each product needs to be converted from product model instances to arrays
-        return [];
+        return values(map([$this, 'productDtoToArray'], $products));
     }
 }
