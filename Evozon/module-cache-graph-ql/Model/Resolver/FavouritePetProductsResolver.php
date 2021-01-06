9b9ec67b44a13a650f1383e5d144b247b8a6eb1e
Bogdan Tomi <bogdan.tomi@evozon.com>
2021-01-06 15:27:19 +0200

    Complete adding the product type to the custom graphQL type

--- Evozon/module-cache-graph-ql/Model/Resolver/FavouritePetProductsResolver.php
+++ Evozon/module-cache-graph-ql/Model/Resolver/FavouritePetProductsResolver.php
@@ -3,6 +3,8 @@
 namespace Evozon\CacheGraphQl\Model\Resolver;
 
 use Magento\Catalog\Api\Data\ProductInterface;
+use Magento\Catalog\Model\Product as ProductOrmModel;
+use Magento\Catalog\Model\ProductFactory as ProductOrmModelFactory;
 use Magento\Catalog\Model\ProductRepository;
 use Magento\Framework\Api\SearchCriteriaBuilder;
 use Magento\Framework\GraphQl\Config\Element\Field;
@@ -28,20 +30,36 @@ class FavouritePetProductsResolver implements ResolverInterface
      * @var DataObjectProcessor
      */
     private DataObjectProcessor $dataObjectProcessor;
+    /**
+     * @var ProductOrmModelFactory
+     */
+    private ProductOrmModelFactory $productOrmFactory;
 
     public function __construct(
         ProductRepository $productRepository,
         SearchCriteriaBuilder $searchCriteriaBuilder,
-        DataObjectProcessor $dataObjectProcessor
+        DataObjectProcessor $dataObjectProcessor,
+        ProductOrmModelFactory $productOrmModelFactory
     ) {
         $this->productRepository = $productRepository;
         $this->searchCriteriaBuilder = $searchCriteriaBuilder;
         $this->dataObjectProcessor = $dataObjectProcessor;
+        $this->productOrmFactory = $productOrmModelFactory;
+    }
+
+    private function createNewProductOrmModel(array $data) : ProductOrmModel
+    {
+        $productModel = $this->productOrmFactory->create(['data' => $data]);
+        $productModel->setId($data['id']);
+        return $productModel;
     }
 
     private function productDtoToArray(ProductInterface $product): array
     {
-        return $this->dataObjectProcessor->buildOutputDataArray($product, ProductInterface::class);
+        $data = $this->dataObjectProcessor->buildOutputDataArray($product, ProductInterface::class);
+        // the model property is required for some core entities (otherwise some properties, like id, cannot be accessed)
+        $data['model'] = $product instanceof ProductOrmModel ? $product : $this->createNewProductOrmModel($data);
+        return $data;
     }
 
     public function resolve(Field $field, $context, ResolveInfo $info, array $value = null, array $args = null)
