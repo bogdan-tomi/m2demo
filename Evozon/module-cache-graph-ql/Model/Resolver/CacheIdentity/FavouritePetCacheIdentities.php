869b7d3ec78e2e4bd423ca819b66c6862f3fcd7a
Bogdan Tomi <bogdan.tomi@evozon.com>
2021-01-08 18:01:16 +0200

    Add custom cache tags to graphQL query

--- Evozon/module-cache-graph-ql/Model/Resolver/CacheIdentity/FavouritePetCacheIdentities.php
+++ Evozon/module-cache-graph-ql/Model/Resolver/CacheIdentity/FavouritePetCacheIdentities.php
@@ -6,8 +6,16 @@ use Magento\Framework\GraphQl\Query\Resolver\IdentityInterface;
 
 class FavouritePetCacheIdentities implements IdentityInterface
 {
+    // the Magento\Framework\DataObject\IdentityInterface should be also implemented by the ORM model
+    // in order for the graphQL query to be refreshed when the entity is changed
+    public const CACHE_TAG = 'fav_p';
+
     public function getIdentities(array $resolvedData): array
     {
-        return ['dummy'];
+        // create an array of tags using unique data on the $resolvedData array
+        $tags = $resolvedData['model'] !== '' ? [self::CACHE_TAG . '_' . $resolvedData['model']] : null;
+
+        // we need to add the entity cache to the array if there are any values (i.e. any set favourite pet)
+        return $tags ? array_merge($tags, [self::CACHE_TAG]) : [];
     }
 }
