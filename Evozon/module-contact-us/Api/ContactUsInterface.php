c56dbfc8835c581592a00406c2a85a28be8aeb94
Bogdan Tomi <bogdan.tomi@evozon.com>
2021-05-20 13:29:22 +0300

    Remove unused method parameters

--- Evozon/module-contact-us/Api/ContactUsInterface.php
+++ Evozon/module-contact-us/Api/ContactUsInterface.php
@@ -6,13 +6,10 @@ namespace Evozon\ContactUs\Api;
 interface ContactUsInterface
 {
     /**
-     * Sends the Contact Us email using the given parameters, returns boolean indicating success
+     * Sends the Contact Us email using the request parameters, returns boolean indicating success
      *
-     * @param string $name
-     * @param string $email
-     * @param string $comment
      * @return bool
      * @author Bogdan Tomi <bogdan.tomi@evozon.com>
      */
-    public function sendMessage($name, $email, $comment);
+    public function sendMessage();
 }
--- Evozon/module-contact-us/Model/Api/ContactUs.php
+++ Evozon/module-contact-us/Model/Api/ContactUs.php
@@ -62,13 +62,10 @@ class ContactUs implements ContactUsInterface
     /**
      * Sends the Contact Us email using the given parameters, returns boolean indicating success
      *
-     * @param string $name
-     * @param string $email
-     * @param string $comment
      * @return bool
      * @author Bogdan Tomi <bogdan.tomi@evozon.com>
      */
-    public function sendMessage($name, $email, $comment)
+    public function sendMessage()
     {
         $result = false;
         try {
